0672965455e4857568180ae4091bc32bdbe51223
Roman Bakakin <60231318@leroymerlin.ru>
2024-05-15 21:34:36 +0300

    test

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -24,6 +24,7 @@ export const api = createApi({
     credentials: 'include',
     headers: {
       'Authorization': getTokenFromLocalStorage() || 'null',
+      'init-data': Telegram.WebApp.initData,
   },
   }),
   endpoints: (builder) => ({
