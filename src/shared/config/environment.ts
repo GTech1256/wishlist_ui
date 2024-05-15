2ffce6a7ffdb4d4451ee7f69c201e782e88612cb
Roman Bakakin <60231318@leroymerlin.ru>
2024-05-15 21:14:06 +0300

    test

--- src/shared/config/environment.ts
+++ src/shared/config/environment.ts
@@ -2,7 +2,7 @@ import { Env } from "types/enums/env";
 import { Environment } from "types/interfaces/environment";
 import { Language } from "types/models";
 import packageInfo from "../../../package.json";
-console.log(import.meta.env)
+// console.log(import.meta.env)
 const env: Env = (import.meta.env.MODE as Env) || Env.Prod;
 
 const ENVIRONMENT: { [key in Env]: Partial<Environment> } = {
@@ -14,19 +14,19 @@ const ENVIRONMENT: { [key in Env]: Partial<Environment> } = {
   },
 };
 
-let initDataLanguage = Language.RU;
-let telegramId = 0;
+const initDataLanguage = Language.RU;
+// let telegramId = 0;
 
-try {
-  const userJsonString = new URLSearchParams(Telegram.WebApp.initData).get("user");
-  const user = JSON.parse(userJsonString || "");
-  initDataLanguage = user.language_code.trim().toUpperCase() || Language.RU;
-  telegramId = user.id;
-} catch (e) {
-  console.error(e);
-}
+// try {
+//   const userJsonString = new URLSearchParams(Telegram.WebApp.initData).get("user");
+//   const user = JSON.parse(userJsonString || "");
+//   initDataLanguage = user.language_code.trim().toUpperCase() || Language.RU;
+//   telegramId = user.id;
+// } catch (e) {
+//   console.error(e);
+// }
 
-console.log({ env }, ENVIRONMENT[env], import.meta.env)
+// console.log({ env }, ENVIRONMENT[env], import.meta.env)
 
 export const environment: Environment = {
   ENV: Env.Prod,
@@ -37,7 +37,7 @@ export const environment: Environment = {
   LANGUAGE: initDataLanguage,
   // GOOGLE_MAPS_API_KEY: "AIzaSyCWT5xNWbh7tB4hilJgiYCiinR8FOqe3l8",
   SENTRY_DSN: null,
-  TELEGRAM_ID: telegramId,
+  TELEGRAM_ID: 0,
   ...ENVIRONMENT[env],
 };
 
